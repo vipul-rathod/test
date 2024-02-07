b0e3bb2fd699ba98c3b7872a5f88f2fe53a03540
Vipul Rathod <vipsravs787@gmail.com>
2024-02-07 11:05:48 +0530

    Update the html css with responsive behaviour.

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Dog License</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/style.css">
     <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
     <script>
       $(function () {
@@ -21,19 +22,6 @@
             });
           });
         </script>
-  <style>
-    div.container-fluid{
-      position:relative;
-      width: 100%;
-      margin: 5px;
-      border: 1px solid #000000;
-    }
-
-    .required::before{
-      content: "*";
-      color: red;
-    }
-  </style>
   </head>
   <body>
     <form id="form">
